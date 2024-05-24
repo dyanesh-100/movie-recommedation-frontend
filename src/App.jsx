6f365a8147c33a43ef78625d408507953b220ac9
Dyanesh D <you@example.com>
2024-05-24 09:31:15 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css'
+import GetAllMoviesComponent from './components/GetAllMovieComponent/GetAllMovieComponent'
+import AddNewMovieComponent from './components/AddNewMovieComponent/AddNewMovieComponent'
+import MovieSuggestionComponent from './components/MovieSuggestionComponent/MovieSuggestionComponent'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+            <div className="container">
+              <h1>Movie Suggestion App</h1>
+              
+            <nav className="nav-menu">
+                <Link to="/" >Home</Link>
+                <Link to="/admin/add" >Add Movies</Link>
+                <Link to="/suggest" >Get Suggestions</Link>
+            </nav>
+           <Routes>
+                 <Route exact path='/' element={<GetAllMoviesComponent/>}></Route>
+                 <Route path='/admin/add' element={<AddNewMovieComponent/>}></Route>
+                 <Route path='/suggest' element={<MovieSuggestionComponent/>}></Route>
+          </Routes>
+          </div>
+       </Router>
   )
 }
 
